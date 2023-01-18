b1b7a3f3bdbb7add394212ce4b2a352ed1c3fafb
Elias Martin <5715@holbertonstudents.com>
2023-01-18 06:38:46 -0800

    fixed two dot and dot dot

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,8 +2,10 @@
 if __name__ == '__main__':
     from sys import argv
 a = (len(argv))
-if a <= 2:
-    print("{} argument.".format(a - 1))
+if a == 2:
+    print("{} argument:".format(a - 1))
+elif a == 1:
+     print("{} arguments.".format(a - 1))
 else:
     print("{} arguments:".format(a - 1))
 for i in range(1, a):
