b6d35e505eb824a40619cd936b50f27e6c5d81cc
Elias Martin <5715@holbertonstudents.com>
2023-01-18 06:33:51 -0800

    fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 if __name__ == '__main__':
     from sys import argv
 a = (len(argv))
-print("{} arguments:".format(a - 1))
+if a <= 2:
+    print("{} argument.".format(a - 1))
+else:
+    print("{} arguments:".format(a - 1))
 for i in range(1, a):
     print("{}: {}".format(i, argv[i]))
