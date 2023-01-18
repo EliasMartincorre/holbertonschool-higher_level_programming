369d400003aecb09eb27173a16fec785430e3b3b
Elias Martin <5715@holbertonstudents.com>
2023-01-18 06:39:59 -0800

    pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ a = (len(argv))
 if a == 2:
     print("{} argument:".format(a - 1))
 elif a == 1:
-     print("{} arguments.".format(a - 1))
+    print("{} arguments.".format(a - 1))
 else:
     print("{} arguments:".format(a - 1))
 for i in range(1, a):
