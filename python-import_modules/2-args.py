eeeaa2a88f047d3780bdc972250d45bc470710f6
Elias Martin <5715@holbertonstudents.com>
2023-01-18 06:28:14 -0800

    argument line command

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     from sys import argv
 a = (len(argv))
-print("{} arguments.".format(a - 1))
+print("{} arguments:".format(a - 1))
 for i in range(1, a):
     print("{}: {}".format(i, argv[i]))
