b2afa081649bc31c35b91b81f42164b91ca605f7
Elias Martin <5715@holbertonstudents.com>
2023-01-21 09:17:49 -0800

    finish

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <opera <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
         a, b, c = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
