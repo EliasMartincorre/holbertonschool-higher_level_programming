611c548ca0d69cfef8d4ec5a647f07efe2264a2a
Elias Martin <5715@holbertonstudents.com>
2023-01-23 08:58:24 -0800

    fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2 and len(tuple_a) > 0:
-        tupleop = tuple_a[0] + tuple_b[0], 0 + tuple_b[1]
+        tupleop = tuple_a[0] + tuple_b[0], tuple_b[1]
     if len(tuple_a) == 0:
         return tuple_b
     if len(tuple_b) < 2 and len(tuple_b) > 0:
@@ -9,6 +9,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tupleop
     if len(tuple_b) == 0:
         return tuple_a
+    if len(tuple_a) == 1 and len(tuple_b) == 1:
+        tupleop = tuple_a[0] + tuple_b[0], 0
     else:
         tupleop = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return tupleop
