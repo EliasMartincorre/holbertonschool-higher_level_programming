66819beeb45b2e4c90c3c1683e532e40afac5414
Elias Martin <5715@holbertonstudents.com>
2023-01-23 09:45:38 -0800

    MAX - interger

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    a = 0
+    a = - 30000
     for item in range(len(my_list)):
         if a < my_list[item]:
             a = my_list[item]
