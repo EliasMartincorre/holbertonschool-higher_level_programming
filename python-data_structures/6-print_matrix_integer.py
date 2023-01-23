fdf75bc6fa0d9db1793742f793911801fc03ad40
Elias Martin <5715@holbertonstudents.com>
2023-01-23 07:42:36 -0800

    print matrix intergers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        return None
+    for row in matrix:
+        for i in range(len(row)):
+            print("{}".format(row[i]), end=" ")
+        print()
