1d453b3c21a89d21e257730d741b8bdd495cb06d
Elias Martin <5715@holbertonstudents.com>
2023-01-24 08:29:24 -0800

    W

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_doctionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     a = 0
     b = ""
