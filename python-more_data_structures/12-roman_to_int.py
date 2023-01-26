6dcbf3980fbf6c079f188e77d52f0117388716dd
Elias Martin <5715@holbertonstudents.com>
2023-01-26 05:23:18 -0800

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string and type(roman_string) != str:
+    if not roman_string or type(roman_string) != str:
         return 0
     r = roman_string
-    a = 0 
+    a = 0
     ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(r)):
         if i > 0 and ro[r[i]] > ro[r[i - 1]]:
