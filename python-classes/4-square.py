d5a47faa1047852b7ceb552898524e83089b85b1
Elias Martin <5715@holbertonstudents.com>
2023-01-31 09:25:05 -0800

    setter and getter

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" this class define a class square"""
+
+
+class Square:
+    """ this attribute define a
+        some value allowed for size,
+        type anda nonnegative"""
+
+    def get__size(self):
+        return self.size
+
+    def set_size(self, value):
+        self.size = value
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.size = size
+
+    def area(self):
+        if type(self.size) != int:
+            raise TypeError('size must be an integer')
+        return (self.size ** 2)
