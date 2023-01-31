7ec2b3a3276c92d0a7ad4e94424d24b6d505ced1
Elias Martin <5715@holbertonstudents.com>
2023-01-31 07:02:26 -0800

    invokes__init__ for the newly created class instance

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This class define a Square"""
+
+
+class Square:
+    """When a class defines an __init__() method,
+    class instantiation automatically invokes __init__()
+          for the newly created class instance """
+    def __init__(self, size):
+        self._Square__size = size
