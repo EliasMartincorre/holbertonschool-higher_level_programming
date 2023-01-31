5b41faeeff9d2e00443ce364fd537daef4f446ad
Elias Martin <5715@holbertonstudents.com>
2023-01-31 08:17:41 -0800

    square size non negative size atribute and type int

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
         type anda nonnegative"""
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError('size must be an interger')
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
