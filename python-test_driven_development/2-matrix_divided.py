3ce53fbe0d6da971ff34372da59c08cb2baf2faf
Elias Martin <5715@holbertonstudents.com>
2023-02-02 08:32:36 -0800

    finish or not

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
+""" this function divide al elements by matrix
+    and return  the result"""
+
+
 def matrix_divided(matrix, div):
+    """test all possible case
+       when succes and when failed"""
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance((div), (int, float)):
         raise TypeError("div must be a number")
     if not isinstance((matrix), (list)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
     if matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix"
+                        " (list of lists) of integers/floats")
     n_m = []
     for i in matrix:
         if len(i) == len(matrix[0]):
-            n_m.append([round(i[r] / div, 2) for r in range(len(i)) if type(i[r]) == int])
+            for a in range(len(i)):
+                if not isinstance((i[a]), (int, float)):
+                    raise TypeError("matrix must be a matrix"
+                                    "(list of lists) of integers/floats")
+            else:
+                n_m.append([round(i[r] / div, 2) for r in range(len(i))])
         else:
             raise TypeError("Each row of the matrix must have the same size")
     return n_m
