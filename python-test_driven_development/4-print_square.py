3dd4b418b26c6b0cacef1348fe49ac2185cafcf8
Elias Martin <5715@holbertonstudents.com>
2023-02-04 05:12:26 -0800

    task

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" define a square, whit size input"""
+
+
 def print_square(size):
+    """ exception for the incorrectly argument"""
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print()
     for i in range(size):
         for i in range(size):
             print("#", end="")
