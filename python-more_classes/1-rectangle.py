a661e802f2cd30f5719ae2b4634518d9ea65d18f
Elias Martin <5715@holbertonstudents.com>
2023-02-05 09:59:20 -0800

    allowed type

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """ Represent a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -14,7 +14,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
