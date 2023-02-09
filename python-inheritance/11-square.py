c2bae365637039f6cd1696de4e0cdba2574691ec
Elias Martin <5715@holbertonstudents.com>
2023-02-09 10:00:51 -0800

    square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Rectangle that inherits from BaseGeometry"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Ins"""
+
+    def __init__(self, size):
+        self.__size = size
+        Rectangle.integer_validator(self, "size", size)
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def __str__(self):
+        string = f"[Rectangle] {self.__size}/{self.__size}"
+        return string
