c7a2201a2d66c298ffc24023fcc3ee476f7d3db6
EliasMartincorre <root@7e3f4dfdda52.(none)>
2023-02-15 10:00:02 -0800

    square inheritance of rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" import module base
+    inherit for rectangle"""
+from models.rectangle import Rectangle
+
+""" class Rectangle for especific
+ comportamiento"""
+
+
+class Square(Rectangle):
+    """ Class rectangle is """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """ information for developer"""
+        p1 = f"[Square] ({self.id}) {self.x}/{self.y}"
+        p2 = f" - {self.size}"
+        return p1 + p2
