4a87d04781f8345cc3acd0b2a3c83b53387ad4c0
EliasMartincorre <113598493+EliasMartincorre@users.noreply.github.com>
2023-02-17 19:42:37 -0300

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,15 +8,16 @@ class Base:
     nb_objects = 0
 
     def __init__(self, id=None):
+        """ahora si"""
         if id is None:
             Base.nb_objects += 1
             self.id = self.nb_objects
         else:
             self.id = id
 
-   def to_json_string(list_dictionaries):
-    """dictionary to json"""
-    a = []
+    def to_json_string(list_dictionaries):
+        """dictionary to json"""
+        a = []
     for i in list_dictionaries:
         a = json.dumps(i)
     return a
