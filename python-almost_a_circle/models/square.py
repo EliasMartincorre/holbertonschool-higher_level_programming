238d0e1d9018b5d0615fbbbba56dcd7f0551757b
Elias Martin <5715@holbertonstudents.com>
2023-02-17 12:56:01 -0800

    kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,6 +28,7 @@ class Square(Rectangle):
         self.__height = value
 
     def update(self, *args, **kwargs):
+        """ update args self attribute"""
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
