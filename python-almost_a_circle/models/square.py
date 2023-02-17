e29a8fcd179c03684f669ca4f584484c5ca89b11
Elias Martin <5715@holbertonstudents.com>
2023-02-17 12:53:21 -0800

    kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,3 +26,15 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         self.__height = value
+
+    def update(self, *args, **kwargs):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = arg[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
