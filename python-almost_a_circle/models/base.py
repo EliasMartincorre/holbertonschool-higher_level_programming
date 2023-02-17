eb379e03caa9b1f686ec1ddc19d71ad2ed15465d
EliasMartincorre <113598493+EliasMartincorre@users.noreply.github.com>
2023-02-17 19:34:36 -0300

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This class will be the “base” of all other classes"""
-
+import json
 
 class Base:
     """only class Base, count number of instance in id = None"""
@@ -13,3 +13,10 @@ class Base:
             self.id = self.nb_objects
         else:
             self.id = id
+
+   def to_json_string(list_dictionaries):
+    """dictionary to json"""
+    a = []
+    for i in list_dictionaries:
+        a = json.dumps(i)
+    return a
