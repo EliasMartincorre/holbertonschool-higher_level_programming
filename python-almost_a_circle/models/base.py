35882f4e4e80bcd908c79e138c8fb28ec3cf0a8c
EliasMartincorre@53d862c47198 <5715@holbertonstudents.com>
2023-02-18 04:00:11 -0800

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,9 +18,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """dictionary to json"""
-        if list_dictionaries is None or list_dictionaries is []:
-            return "[]"
-        a = []
-        for i in list_dictionaries:
-            a = json.dumps(i)
-        return str(a)
+        if list_dictionaries is None:
+            s = "[]"
+            return s
+        else:
+            return json.dumps(list_dictionaries)
+       
