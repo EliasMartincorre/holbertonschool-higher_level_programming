a8ab156f5a0b9f88abbb1998c0ac95608f047db4
EliasMartincorre <martincorrea792@gmail.com>
2023-06-03 11:47:49 -0300

    comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(0, 10):
     for a in range(0, 10):
         if (i == 8 and a == 9):
-            print(f"{i}{a}")
+            print("{}{}".format(i, a))
         elif (i < a):
-            print(f"{i}{a}, ", end="")
+            print("{}{}, ".format(i, a), end="")
