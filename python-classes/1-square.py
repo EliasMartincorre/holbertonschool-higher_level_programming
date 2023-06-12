1e88559ebf559c017f97e2cb548ca795778d9d70
EliasMartincorre <martincorrea792@gmail.com>
2023-06-12 12:56:14 -0300

    private instance atributte

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+class Square:
+    """ this is a empty class
+        for started  to work about
+        class
+    """
+    def __init__(self, size):
+        """the metodo init inicializa una clase con
+        ciertos valores, pero en este caso se crea
+        el atributo size como privado
+        para que pertenezca a la clase
+        """
+        self.__size = size
