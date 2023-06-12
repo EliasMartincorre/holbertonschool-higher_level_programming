30534fbebcc0b770177b2d77779a30d60a71b655
EliasMartincorre <martincorrea792@gmail.com>
2023-06-12 14:00:43 -0300

    ii

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+class Square:
+    """ this is a empty class
+        for started  to work about
+        class
+    """
+    def __init__(self, size=0):
+        """the metodo init inicializa una clase con
+        ciertos valores, pero en este caso se crea
+        el atributo size como privado
+        para que pertenezca a la clase
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+    
+    def area(self):
+        return (self.__size * self.__size)
