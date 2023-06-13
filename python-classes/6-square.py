b4f6f262143666287ab631cd1492179e77bc1c50
EliasMartincorre <martincorrea792@gmail.com>
2023-06-13 16:05:57 -0300

    self.size

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""In this program I created a class with the name
-    Square then I`m saving the different features
-    that describe a square. 
-"""
+""" this class define a class square"""
 
 
 class Square:
-    """ this is a empty class
-        for started  to work about
-        class
+    """ this attribute define a
+        some value allowed for size,
+        type anda nonnegative"""
 
-        in the start of this class
-        evoke an constructor __init__, 
-        the class allow  likes input, two arguments.
-        Se hace uso de los decoradores @property y  @setter para 
-        reasignar un nuevo valor a una variable creada como privada.
-
-
-    """
     def __init__(self, size=0, position=(0, 0)):
-        """the metodo init inicializa una clase con
-        ciertos valores, pero en este caso se crea
-        el atributo size como privado
-        para que pertenezca a la clase
-        """
-        self.__size = size
+        self.size = size
         self.__position = position
 
     @property
@@ -34,33 +18,24 @@ class Square:
     @size.setter
     def size(self, value):
         if type(value) != int:
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
     def area(self):
-        """ Este es un metodo
-        de instancia que toma el valor
-        de size devuelve el cuadrado.
-            """
         return (self.__size ** 2)
 
     def my_print(self):
-        """
-            print in stdout a
-            graphic representation of
-            square with character #
-            """
         if self.__size == 0:
             print()
-        for b in range(0, self.__position[1]):
+        for i in range(0, self.__position[1]):
             print()
-        for i in range(self.size):
-            for c in range(self.__position[0]):
-                print("", end=" ")
-            for a in range(self.__size):
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                print(" ", end="")
+            for i in range(self.__size):
                 print("#", end="")
             print()
 
