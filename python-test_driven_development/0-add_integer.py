a1c95012b119fdd206c5abe78a6dee94b7d6a09d
EliasMartincorre <martincorrea792@gmail.com>
2023-06-14 13:36:15 -0300

    tests and more

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Keyword arguments:
+argument -- adition
+Return: adition of a and b
+"""
+def add_integer(a, b=98):
+    if (type(a) != int) and (type(a) != float):
+        raise TypeError("a must be an integer")
+    elif (type(b) != int) and (type(b) != float):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
