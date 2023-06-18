e0b4820581f2eb2f12931bdd97c8699db8de115b
EliasMartincorre <martincorrea792@gmail.com>
2023-06-18 13:39:01 -0300

    type(self)

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        number_of_instance += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -68,5 +68,5 @@ class Rectangle:
         return ("Rectangle(2, 4)")
 
     def __del__(self):
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
