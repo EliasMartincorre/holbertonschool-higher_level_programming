7acf84582f5eb44a74331fad4b6a109382c6da14
EliasMartincorre <martincorrea792@gmail.com>
2023-06-18 11:33:46 -0300

    6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -19,6 +19,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertIsInstance(max_integer([3, 3, 4]), int)
         self.assertEqual(max_integer([85, 2, 4]), 85)
 
+    def test_error_maxinteger(self):
+        self.assertRaises(TypeError, max_integer, ["F", 2, 3])
+        self.assertRaises(KeyError, max_integer, {'r': 4, '4': 5})
+
 
 if __name__ == '__main__':
 
