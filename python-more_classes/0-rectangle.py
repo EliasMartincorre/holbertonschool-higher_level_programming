68e63d82946ef26fedbad2f2d2dd5730678bc6f6
EliasMartincorre <martincorrea792@gmail.com>
2023-06-18 11:44:53 -0300

    first

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+class Rectangle:
+    """
+        only pass
+    """
+
+    pass
