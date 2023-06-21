a54bb42e0d5f6f25d7bca67456f328caa21ebea0
EliasMartincorre <martincorrea792@gmail.com>
2023-06-21 17:43:43 -0300

    is the same

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that returns True
+if the object is exactly
+an instance of the specified
+class ; otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """ return True for instance
+    otherwise fale.
+    """
+
+    return isinstance(obj, a_class)
