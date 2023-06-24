f874dd1a0a10a20a9a82a443acf14d714fec6d06
EliasMartincorre <martincorrea792@gmail.com>
2023-06-24 11:22:50 -0300

    deserialization

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pythorn3
+"""
+argument -- object.json
+Return: object of python
+"""
+from json import loads
+
+
+def from_json_string(my_str):
+    """
+    Return: object of python
+    """
+
+    return (loads(my_str))
