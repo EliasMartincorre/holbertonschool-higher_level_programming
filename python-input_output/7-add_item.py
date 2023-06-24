5709540c8f5fc7732151f98478b66780b96f3c76
EliasMartincorre <martincorrea792@gmail.com>
2023-06-24 12:19:44 -0300

    by element

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -15,7 +15,8 @@ for i in range(1, len(argv)):
 
 if os.path.exists(filename):
     myobjeto = load_from_json_file(filename)
-    myobjeto.append(a)
+    for i in range(len(a)):
+        myobjeto.append(a[i])
     save_to_json_file(myobjeto, filename)
 else:
     save_to_json_file(a, filename)
