3ea736fb6a8735b103b785a359c7168bfb08c3e6
EliasMartincorre <martincorrea792@gmail.com>
2023-06-24 10:00:08 -0300

    correct shebang

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythoon3
+#!/usr/bin/python3
 """
  function that reads a text
  file (UTF8) and prints it to stdout:
@@ -9,11 +9,12 @@ def read_file(filename=""):
     """
     Return: all character in the
     file.
-
+    """
     with open(filename, 'r', encoding="UTF8") as f:
         tostdout = f.read()
         print(tostdout, end='')
+
     """
     with open(filename, 'r', encoding="UTF8") as f:
         for lines in f:
-            print(lines, end="")
+            print(lines, end="")"""
