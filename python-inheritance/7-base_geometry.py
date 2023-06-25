95026df4481c4c8f3b193f616fc951d9e029bbd9
EliasMartincorre <martincorrea792@gmail.com>
2023-06-25 10:43:46 -0300

    last try

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         "only int"
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
