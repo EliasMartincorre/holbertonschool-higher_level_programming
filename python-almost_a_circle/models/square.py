9400180f029baf25a49d905386f329a04ee96f1e
EliasMartincorre <martincorrea792@gmail.com>
2023-07-01 14:01:23 -0300

    super

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} {self.size}"
