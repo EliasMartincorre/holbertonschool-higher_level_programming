1915ea6cc570d7e5dd58ca49e3c3d59d11ffa0a5
EliasMartincorre <martincorrea792@gmail.com>
2023-07-01 14:04:50 -0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """clase que hereda de
+    la clase rectangulo que a su vez
+    hereda de ka ckase base,
+    se utiliza el metodo super.__init__ para
+    utilizar los elementos de la clase
+    base."""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
