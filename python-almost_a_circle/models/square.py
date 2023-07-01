5157b0dbb7f92dab06120bb3732e652b550acca2
EliasMartincorre <martincorrea792@gmail.com>
2023-07-01 14:18:24 -0300

    setter and getter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,5 +19,13 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
