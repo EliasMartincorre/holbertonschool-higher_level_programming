ee9f3bf0df930c27f86d48b59be5f7ac4351b94b
EliasMartincorre <martincorrea792@gmail.com>
2023-07-02 09:18:29 -0300

    args, kwargs. update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -29,3 +29,22 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        """update es una funcion que
+        asigna un nuevo valor
+        a los atributos de una funcion,
+        primero utilizando args, y despues
+        utilizando el par-llave
+        valor"kwargs"
+        """
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.size = args[1]
+        if len(args) == 3:
+            self.x = args[2]
+        if len(args) == 4:
+            self.y = args[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
