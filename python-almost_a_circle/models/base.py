748ae8752f84c8a2ffa2603a5c2e7f5fca974041
EliasMartincorre <martincorrea792@gmail.com>
2023-07-02 12:53:31 -0300

    in case of fail ?

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -64,9 +64,13 @@ class Base:
         argument -- description
         Return: return_description
         """
-        try:
-           dummy = cls(1, 1)
-           dummy.update(**dictionary)
-           return dummy
-        except Exception:
-            pass
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        if dictionary is None:
+            return
+        else:
+            dummy.update(**dictionary)
+        return dummy
+
