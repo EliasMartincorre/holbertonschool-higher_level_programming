863ee5c587536feaeb31a82a5bb261790f64aa2a
EliasMartincorre <martincorrea792@gmail.com>
2023-07-02 14:32:12 -0300

    error solved

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,17 +32,17 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """update es una funcion que
-        asigna un nuevo valor
-        a los atributos de una funcion,
-        primero utilizando args, y despues
-        utilizando el par-llave
-        valor"kwargs"
-        """
-        if len(args) == 1:
+           asigna un nuevo valor
+           a los atributos de una funcion,
+           primero utilizando args, y despues
+           utilizando el par-llave
+           valor"kwargs"
+           """
+        if len(args) >= 1:
             self.id = args[0]
-        if len(args) == 2:
+        if len(args) >= 2:
             self.size = args[1]
-        if len(args) => 3:
+        if len(args) >= 3:
             self.x = args[2]
         if len(args) == 4:
             self.y = args[3]
